d072d55935d91a27e2d26c52d75458bc7fbc9b66
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-05-04 16:35:09 +0300

    add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import "./App.scss";
+// import "./App.scss";
+import { Routes, Route, Outlet } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import Container from "@mui/material/Container";
 import { Header } from "./components/Header/Header";
+
+const Home = lazy(() => import("./pages/Home"));
+const FullPost = lazy(() => import("./pages/FullPost"));
+const AddPost = lazy(() => import("./pages/AddPost/AddPost"));
+const Login = lazy(() => import("./pages/Login/Login"));
+const Registration = lazy(() => import("./pages/Registration/Registration"));
 
 function App() {
   return (
     <>
       <Header />
 
-      <h1> React Blog</h1>
+      <Container maxWidth="lg">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/posts/:id" element={<FullPost />} />
+            <Route path="/add-post" element={<AddPost />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Registration />} />
+          </Routes>
+          <Outlet />
+        </Suspense>
+      </Container>
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import Container from "@mui/material/Container";
 import FeedIcon from "@mui/icons-material/Feed";
 import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 import styles from "./Header.module.scss";
 
 export const Header = () => {
@@ -12,16 +13,16 @@ export const Header = () => {
     <div className={styles.root}>
       <Container maxWidth="lg">
         <div className={styles.inner}>
-          <a className={styles.logo} href="/">
+          <Link className={styles.logo} to="/">
             <FeedIcon />
             MPS BLOG
-          </a>
+          </Link>
           <div className={styles.buttons}>
             {isAuth ? (
               <>
-                <a href="/posts/create">
+                <Link to="/posts/create">
                   <Button variant="contained">Write an article</Button>
-                </a>
+                </Link>
                 <Button
                   onClick={onClickLogout}
                   variant="contained"
@@ -32,12 +33,12 @@ export const Header = () => {
               </>
             ) : (
               <>
-                <a href="/login">
+                <Link to="/login">
                   <Button variant="outlined">Login</Button>
-                </a>
-                <a href="/register">
+                </Link>
+                <Link to="/register">
                   <Button variant="contained">Register</Button>
-                </a>
+                </Link>
               </>
             )}
           </div>
