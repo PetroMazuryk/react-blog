56800184d85a12a14071be8ab72a4edaa70437cc
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-07-27 23:17:29 +0300

    added favicon

--- src/pages/AddPost/AddPost.jsx
+++ src/pages/AddPost/AddPost.jsx
@@ -1,38 +1,38 @@
-import React from "react";
+// import React from "react";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
-import SimpleMDE from "react-simplemde-editor";
+// import SimpleMDE from "react-simplemde-editor";
 
 import "easymde/dist/easymde.min.css";
 import styles from "./AddPost.module.scss";
 
 const AddPost = () => {
   const imageUrl = "";
-  const [value, setValue] = React.useState("");
+  // const [value, setValue] = React.useState("");
 
   const handleChangeFile = () => {};
 
   const onClickRemoveImage = () => {};
 
-  const onChange = React.useCallback((value) => {
-    setValue(value);
-  }, []);
+  // const onChange = React.useCallback((value) => {
+  //   setValue(value);
+  // }, []);
 
-  const options = React.useMemo(
-    () => ({
-      spellChecker: false,
-      maxHeight: "400px",
-      autofocus: true,
-      placeholder: "Введіть текст...",
-      status: false,
-      autosave: {
-        enabled: true,
-        delay: 1000,
-      },
-    }),
-    []
-  );
+  // const options = React.useMemo(
+  //   () => ({
+  //     spellChecker: false,
+  //     maxHeight: "400px",
+  //     autofocus: true,
+  //     placeholder: "Введіть текст...",
+  //     status: false,
+  //     autosave: {
+  //       enabled: true,
+  //       delay: 1000,
+  //     },
+  //   }),
+  //   []
+  // );
 
   return (
     <Paper style={{ padding: 30 }}>
@@ -66,12 +66,12 @@ const AddPost = () => {
         placeholder="Теги"
         fullWidth
       />
-      <SimpleMDE
+      {/* <SimpleMDE
         className={styles.editor}
         value={value}
         onChange={onChange}
         options={options}
-      />
+      /> */}
       <div className={styles.buttons}>
         <Button size="large" variant="contained">
           Опублікувати
